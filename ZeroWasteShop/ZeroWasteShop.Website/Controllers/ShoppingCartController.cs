aada451be04a4fa14c7163ec403a92161f12efe1
geoGitAm <71641416+geoGitAm@users.noreply.github.com>
2021-02-07 01:19:20 +0300

    added  ShoppingCartController, ShoppingCartViewModel, ShoppingCart Index view.

---
+++ ZeroWasteShop/ZeroWasteShop.Website/Controllers/ShoppingCartController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZeroWasteShop.Website.Models;
+using ZeroWasteShop.Website.ViewModels;
+
+namespace ZeroWasteShop.Website.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartController(IProductRepository productRepository, ShoppingCart shoppingCart)
+        {
+            _productRepository = productRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public ViewResult Index()
+        {
+            _shoppingCart.ShoppingCartItems = _shoppingCart.GetShoppingCartItems();
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(shoppingCartViewModel);
+        }
+
+        public RedirectToActionResult AddToShoppingCart(int productId)
+        {
+            var selectedProduct = _productRepository.GetAllProducts
+                .FirstOrDefault(c => c.ProductId == productId);
+
+            if (selectedProduct != null)
+            {
+                _shoppingCart.AddToCart(selectedProduct, 1);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult RemoveFromShoppingCart(int productId)
+        {
+            var selectedProduct = _productRepository.GetAllProducts
+                .FirstOrDefault(c => c.ProductId == productId);
+
+            if (selectedProduct != null)
+            {
+                _shoppingCart.RemoveFromCart(selectedProduct);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
