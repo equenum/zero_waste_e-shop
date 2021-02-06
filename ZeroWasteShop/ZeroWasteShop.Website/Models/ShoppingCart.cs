41aaef8ba03d8922ad0775bf10b658f47d871d46
geoGitAm <71641416+geoGitAm@users.noreply.github.com>
2021-02-07 00:46:08 +0300

    updated ShoppingCart.

--- ZeroWasteShop/ZeroWasteShop.Website/Models/ShoppingCart.cs
+++ ZeroWasteShop/ZeroWasteShop.Website/Models/ShoppingCart.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ namespace ZeroWasteShop.Website.Models
     public class ShoppingCart
     {
         private readonly AppDbContext _appDbContext;
+
         public string ShoppingCartId { get; set; }
         public List<ShoppingCartItem> ShoppingCartItems { get; set; }
 
@@ -27,6 +29,82 @@ namespace ZeroWasteShop.Website.Models
             session.SetString("CartId", cartId);
 
             return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+
+        public void AddToCart(Product product, int amount)
+        {
+            var shoppingCartItem = _appDbContext.ShoppingCartItems
+                .SingleOrDefault(
+                s => s.Product.ProductId == product.ProductId && s.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Product = product,
+                    Amount = amount
+                };
+
+                _appDbContext.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+
+            _appDbContext.SaveChanges();
+        }
+
+        public int RemoveFromCart(Product product)
+        {
+            var shoppingCartItem = _appDbContext.ShoppingCartItems
+                .SingleOrDefault(
+                s => s.Product.ProductId == product.ProductId && s.ShoppingCartId == ShoppingCartId);
+
+            var localAmount = 0;
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                    localAmount = shoppingCartItem.Amount;
+                }
+                else
+                {
+                    _appDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+
+            _appDbContext.SaveChanges();
+
+            return localAmount;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _appDbContext.ShoppingCartItems
+                .Where(c => c.ShoppingCartId == ShoppingCartId)
+                .Include(s => s.Product)
+                .ToList());
+        }
+
+        public void ClearCart()
+        {
+            var cartItems = _appDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId);
+
+            _appDbContext.ShoppingCartItems.RemoveRange(cartItems);
+            _appDbContext.SaveChanges();
+        }
+
+        public decimal GetShoppingCartTotal()
+        {
+            var total = _appDbContext.ShoppingCartItems
+                .Where(c => c.ShoppingCartId == ShoppingCartId)
+                .Select(c => c.Product.Price * c.Amount).Sum();
+
+            return total;
         }
     }
 }
--- ZeroWasteShop/ZeroWasteShop.Website/Startup.cs
+++ ZeroWasteShop/ZeroWasteShop.Website/Startup.cs
@@ -32,6 +32,10 @@ namespace ZeroWasteShop.Website
             services.AddControllersWithViews();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ShoppingCart>(sc => ShoppingCart.GetCart(sc));
+
+            services.AddHttpContextAccessor();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +48,8 @@ namespace ZeroWasteShop.Website
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
