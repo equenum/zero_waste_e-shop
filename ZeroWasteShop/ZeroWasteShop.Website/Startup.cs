6ce8943b598822c6931330074f92ca59dafb9b1a
geoGitAm <71641416+geoGitAm@users.noreply.github.com>
2021-02-07 22:55:09 +0300

    added UseAuthentication of Startup Configure.

--- ZeroWasteShop/ZeroWasteShop.Website/Startup.cs
+++ ZeroWasteShop/ZeroWasteShop.Website/Startup.cs
@@ -52,6 +52,7 @@ namespace ZeroWasteShop.Website
             app.UseSession();
 
             app.UseRouting();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
